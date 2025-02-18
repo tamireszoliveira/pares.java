f4514887e784e273c4339bcb8b2c64b0577badc5
Tami Oliveira <tamiresaldeoliveira@gmail.com>
2025-02-18 10:21:58 -0300

    controller dev

--- src/controller/ParesController.java
+++ src/controller/ParesController.java
@@ -1,5 +1,20 @@
 package controller;
 
 public class ParesController {
+    public int fpares(int vetor[]) {
+        return fparesRecursivo(vetor, vetor.length);
+    }
 
+    public int fparesRecursivo(int vetor[], int tamanho) {
+        if (tamanho == 0) {
+            return 0;
+        } else {
+            int ultimoElemento = vetor[tamanho - 1];
+            if (ultimoElemento % 2 == 0) {
+                return 1 + fparesRecursivo(vetor, tamanho - 1);
+            } else {
+                return fparesRecursivo(vetor, tamanho - 1);
+            }
+        }
+    }
 }
--- src/view/ParesMain.java
+++ src/view/ParesMain.java
@@ -1,5 +1,21 @@
 package view;
 
-public class ParesMain {
+import javax.swing.JOptionPane;
 
+import controller.ParesController;
+
+public class ParesMain {
+	public static void main(String args[]) {
+			/*3. Construir uma função recursiva que receba um vetor e seu tamanho e apresente a quan�dade de
+	números pares existentes no vetor. Considere que a entrada deve ser, apenas de números naturais
+	diferentes de zero.*/
+		int tamanho = Integer.parseInt(JOptionPane.showInputDialog("Insira o tamanho do vetor"));
+		int vetor[]= new int [tamanho];
+		for(int i = 0; i< vetor.length; i++) {
+			vetor[i] = Integer.parseInt(JOptionPane.showInputDialog("Preencha o vetor"));
+		}
+		ParesController par = new ParesController();
+		int pares = par.fpares(vetor);
+		System.out.println(pares); 
+	}
 }
